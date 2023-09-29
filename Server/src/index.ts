2d6d7bd8690872727cf585dd96095040865b761c
Elad Levi <60574244+eladlevi013@users.noreply.github.com>
2023-09-29 16:04:52 +0300

    updated package.json and .tsconfig

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { createServer } from 'http';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 import { setupSocketIO } from './socket';
 dotenv.config();
 
