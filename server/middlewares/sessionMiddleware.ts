01d1bf86bb17b830e97e4593d87e5986c884e89b
eladlevi013 <eladlevi013@gmail.com>
2023-10-06 14:18:36 +0300

    cookie different settings

--- server/middlewares/sessionMiddleware.ts
+++ server/middlewares/sessionMiddleware.ts
@@ -5,6 +5,6 @@ export default session({
     secret: process.env.SESSION_SECRET_KEY as string,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true, httpOnly: true, maxAge: 2592000000 },
+    cookie: { secure: true, httpOnly: true, maxAge: 2592000000, sameSite: 'none' },
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
 })
