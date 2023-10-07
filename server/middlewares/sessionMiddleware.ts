3b8529a4a27ba3014fbeb84b5484a9790ea38a66
eladlevi013 <eladlevi013@gmail.com>
2023-10-07 20:12:18 +0300

    making express-session work on localhost

--- server/middlewares/sessionMiddleware.ts
+++ server/middlewares/sessionMiddleware.ts
@@ -3,14 +3,14 @@ import MongoStore from 'connect-mongo'
 
 export default session({
     secret: process.env.SESSION_SECRET_KEY as string,
-    resave: false,
-    saveUninitialized: false,
+    resave: true,
+    saveUninitialized: true,
     name: 'sessionServer',
     cookie: {
         secure: process.env.PRODUCTION === 'true' ? true : false,
         httpOnly: true,
         maxAge: 2592000000,
-        sameSite: 'none',
+        sameSite: process.env.PRODUCTION === 'true' ? 'none' : 'lax',
         },
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
 })
