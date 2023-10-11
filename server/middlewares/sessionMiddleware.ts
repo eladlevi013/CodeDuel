7d492ecb81530b79fdee54dc011120b217cd57f2
eladlevi013 <eladlevi013@gmail.com>
2023-10-11 16:10:35 +0300

    redis implemented for express-session

--- server/middlewares/sessionMiddleware.ts
+++ server/middlewares/sessionMiddleware.ts
@@ -1,16 +1,36 @@
-import session from 'express-session'
-import MongoStore from 'connect-mongo'
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import RedisStore from 'connect-redis';
+import { createClient } from 'redis';
 
-export default session({
+const getStore = (): session.Store => {
+    if (process.env.PRODUCTION === 'true') {
+        return new MongoStore({ mongoUrl: process.env.MONGO_URL });
+    } else {
+        // Initialize Redis client.
+        let redisClient = createClient();
+        redisClient.connect().catch(console.error);
+
+        // Initialize and return Redis store.
+        return new (RedisStore as any)({
+            client: redisClient,
+            prefix: 'codeduel:'
+        });        
+    }
+};
+
+const sessionConfig: session.SessionOptions = {
     secret: process.env.SESSION_SECRET_KEY as string,
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
     name: 'sessionServer',
     cookie: {
         secure: process.env.PRODUCTION === 'true' ? true : false,
         httpOnly: true,
         maxAge: 2592000000,
         sameSite: process.env.PRODUCTION === 'true' ? 'none' : 'lax',
-        },
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
-})
+    },
+    store: getStore(),
+};
+
+export default session(sessionConfig);
